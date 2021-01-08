20e819f816d5051ee3606c200dae265d0817d076
EC2 Default User <ec2-user@ip-172-31-33-85.us-east-2.compute.internal>
2021-01-08 00:37:43 +0000

    third commit

---
+++ main.js
@@ -0,0 +1,65 @@
+'use strict';
+
+{
+    const timer = document.getElementById('timer');
+    const start = document.getElementById('start');
+    const stop = document.getElementById('stop');
+    const reset = document.getElementById('reset');
+    
+    let startTime;
+    let timeoutId
+    let elapsedTime = 0;
+    
+    function countUp() {
+        
+        const d = new Date(Date.now() - startTime + elapsedTime);
+        const h = d.getUTCHours();
+        const m = d.getMinutes();
+        const s = d.getSeconds();
+        const ms = d.getMilliseconds();
+        timer.textContent = `${h}:${m}:${s}:${ms}`;
+        
+        timeoutId = setTimeout(() => {
+            countUp();
+        }, 10);
+    }
+    
+    function setButtonStateInitial() {
+        start.disabled = false;
+        stop.disabled = true;
+        reset.disabled = true;
+    }
+    
+    function setButtonStateRunning() {
+        start.disabled = true;
+        stop.disabled = false;
+        reset.disabled = true;
+    }
+    
+    function setButtonStateStopped() {
+        start.disabled = false;
+        stop.disabled = true;
+        reset.disabled = false;
+    }
+    
+    setButtonStateInitial();
+
+    
+    start.addEventListener('click', () => {
+        setButtonStateRunning()
+        startTime = Date.now();
+        countUp();
+    })
+    
+    stop.addEventListener('click', () => {
+        setButtonStateStopped()
+        clearTimeout(timeoutId)
+        elapsedTime += Date.now() - startTime;
+    });
+
+    reset.addEventListener('click', () => {
+        setButtonStateInitial()
+        timer.textContent = '0:0:0:0';
+        elapsedTime = 0 ;
+    });
+}
